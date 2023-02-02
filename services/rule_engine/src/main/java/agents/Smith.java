1e736c7b9f1724b09d6d2ab956ebc4dc632f3bae
KarloVis <37339769+KarloVis@users.noreply.github.com>
2023-02-02 21:05:54 +0100

    Added function to process the spreadsheet

--- services/rule_engine/src/main/java/agents/Smith.java
+++ services/rule_engine/src/main/java/agents/Smith.java
@@ -1,14 +1,27 @@
 package agents;
 
+import java.time.LocalDateTime;
+
+import com.mindsmiths.gsheetsAdapter.reply.Spreadsheet;
 import com.mindsmiths.ruleEngine.model.Agent;
+import com.mindsmiths.ruleEngine.util.Log;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
 
 @Data
 @ToString(callSuper = true)
+@AllArgsConstructor
 public class Smith extends Agent {
+
+    public LocalDateTime lastTableUpdate = null;
 
     public Smith() {
         this.id = "SMITH";
+    }
+
+    public void processSheet(Spreadsheet spreadsheet){
+        Log.info(spreadsheet);
     }
 }
