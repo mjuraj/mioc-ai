b0aa89d4159d2af849ca2a1402d0384a4d896d9a
svebaa <sveba.vasic@gmail.com>
2023-02-04 20:49:07 +0100

    initial commit

--- services/rule_engine/src/main/java/agents/Moli.java
+++ services/rule_engine/src/main/java/agents/Moli.java
@@ -2,11 +2,17 @@ package agents;
 
 import com.mindsmiths.ruleEngine.model.Agent;
 import lombok.*;
+import com.mindsmiths.ruleEngine.util.Log;
 
 import com.mindsmiths.armory.ArmoryAPI;
 import com.mindsmiths.armory.Screen;
 
 import com.mindsmiths.armory.component.Title;
+
+import java.io.IOException;
+import java.util.List;
+import com.mindsmiths.emailAdapter.NewEmail;
+import com.mindsmiths.emailAdapter.EmailAdapterAPI;
 
 @Data
 @ToString(callSuper = true)
@@ -18,5 +24,20 @@ public class Moli extends Agent {
             new Screen ("Hello")
                 .add(new Title("Hello world!"))
         );
+        try {
+            sendEmail(List.of("sveba.vasic@gmail.com"), "Test title", "Test 1234");
+            Log.info("Mail uspjesno poslan!");
+        } catch(IOException ex) {
+            Log.info("Exception! " + ex.toString());
+        }
+    }
+
+    public void sendEmail(List<String> recipients, String emailTitle, String emailText) throws IOException {
+        NewEmail email = new NewEmail();
+        email.setRecipients(recipients);
+        email.setSubject(emailTitle);
+        email.setPlainText(emailText);
+
+        EmailAdapterAPI.newEmail(email);
     }
 }
