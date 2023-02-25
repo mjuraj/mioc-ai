645dbc5855f4c36bcdd929adfd218e0239a0acab
KarloVis <37339769+KarloVis@users.noreply.github.com>
2023-02-25 21:44:42 +0100

    Changed some variable names

--- services/rule_engine/src/main/java/agents/Smith.java
+++ services/rule_engine/src/main/java/agents/Smith.java
@@ -23,8 +23,8 @@ import lombok.ToString;
 public class Smith extends Agent {
 
     LocalDateTime lastTableUpdate = null;
-    int nextRow = 0;
-    int odgNext = 0;
+    int nextMailRow = 0;
+    int nextAnsRow = 0;
     Spreadsheet sheet;
 
     // Agent id
@@ -33,12 +33,12 @@ public class Smith extends Agent {
     }
 
     // Getting the sheet as an object and getting the first free row in the spreadsheet
-    public void processSheet(Spreadsheet spreadsheet){
+    public void processSpreadsheet(Spreadsheet spreadsheet){
         sheet = spreadsheet;
-        List<Map<String, String>> sheetData = spreadsheet.getSheets().get("Mailovi");
-        List<Map<String, String>> odg = spreadsheet.getSheets().get("Odgovori");
-        nextRow = sheetData.size();
-        odgNext = odg.size();
+        List<Map<String, String>> emails = spreadsheet.getSheets().get("Mailovi");
+        List<Map<String, String>> answers = spreadsheet.getSheets().get("Odgovori");
+        nextMailRow = emails.size() + 2;
+        nextAnsRow = answers.size() + 2;
     }
 
     /* 
@@ -55,17 +55,17 @@ public class Smith extends Agent {
 
     // Function to update "Odgovori" sheet with given parameters
     // TODO: set the function up to work with data collected from armory
-    public void getReview(String mail, String ocjena, String review){
-        String range = String.format("Odgovori!A%d:C%d",odgNext+2,odgNext+2);
+    public void getReview(String mail, String grade, String review){
+        String range = String.format("Odgovori!A%d:C%d", nextAnsRow, nextAnsRow);
         List<List<String>> values = List.of(List.of(mail,ocjena,review));
         GSheetsAdapterAPI.updateSheet(values, range);
     }
 
     // Going through the list of mails in the spreadsheet and creating agents for mails who don't already have one
-    public void newAgent(){
-        List<Map<String, String>> popis = sheet.getSheets().get("Mailovi");
+    public void createNewAgents(){
+        List<Map<String, String>> mailList = sheet.getSheets().get("Mailovi");
 
-        for (Map<String, String> item : popis) {
+        for (Map<String, String> item : mailList) {
             for (String key : item.keySet()) {
                 String email = item.get(key);
                 if(!Agents.exists(email)){
