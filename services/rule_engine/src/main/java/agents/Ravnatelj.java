ca9f95ca9e0a1895e08a7f9a3e4de04aaa124344
markozele1 <marko.zelenovicc@gmail.com>
2023-02-26 14:49:22 +0100

    napravio agent ravnatelja, treba dovrsiti gdje su komentari

---
+++ services/rule_engine/src/main/java/agents/Ravnatelj.java
@@ -0,0 +1,25 @@
+package agents;
+
+import com.mindsmiths.ruleEngine.model.Agent;
+
+import java.io.IOException;
+import java.util.List;
+import com.mindsmiths.emailAdapter.NewEmail;
+import com.mindsmiths.emailAdapter.EmailAdapterAPI;
+
+public class Ravnatelj extends Agent {
+    public static String ID = "RAVNATELJ";
+
+    public Ravnatelj() {
+        id = ID;
+    }
+
+    public void sendEmail(List<String> recipients, String emailTitle, String emailText) throws IOException {
+        NewEmail email = new NewEmail();
+        email.setRecipients(recipients);
+        email.setSubject(emailTitle);
+        email.setPlainText(emailText);
+
+        EmailAdapterAPI.newEmail(email);
+    }
+}
