c9a628ff592b2c28a3064536c8c7825cba478282
KarloVis <37339769+KarloVis@users.noreply.github.com>
2023-02-27 10:31:40 +0100

    cleaned up the code

--- services/rule_engine/src/main/java/agents/Smith.java
+++ services/rule_engine/src/main/java/agents/Smith.java
@@ -23,9 +23,7 @@ import lombok.ToString;
 public class Smith extends Agent {
 
     LocalDateTime lastTableUpdate = null;
-    int nextMailRow = 0;
     int nextAnsRow = 0;
-    Spreadsheet sheet;
 
     // Agent id
     public Smith() {
@@ -34,39 +32,19 @@ public class Smith extends Agent {
 
     // Getting the sheet as an object and getting the first free row in the spreadsheet
     public void processSpreadsheet(Spreadsheet spreadsheet){
-        sheet = spreadsheet;
-        List<Map<String, String>> emails = spreadsheet.getSheets().get("Mailovi");
         List<Map<String, String>> answers = spreadsheet.getSheets().get("Odgovori");
-        nextMailRow = emails.size() + 2;
         nextAnsRow = answers.size() + 2;
-    }
 
-    /* 
-
-    OPTIONAL - Function which updates the "Mailovi" sheet with a mock mail for now
-
-    public void updateGSheet() {
-        List<List<String>> values = List.of(List.of("jedan@mail.com"));
-        String range = String.format("Mailovi!A%d",nextRow+2);
-        GSheetsAdapterAPI.updateSheet(values, range);
-    }
-
-    */
-
-    // Function to update "Odgovori" sheet with given parameters
-    // TODO: set the function up to work with data collected from armory
-    public void getReview(String mail, String grade, String review){
-        String range = String.format("Odgovori!A%d:C%d", nextAnsRow, nextAnsRow);
-        List<List<String>> values = List.of(List.of(mail,grade,review));
-        GSheetsAdapterAPI.updateSheet(values, range);
+        createNewAgents(spreadsheet);
     }
 
     // Going through the list of mails in the spreadsheet and creating agents for mails who don't already have one
-    public void createNewAgents(){
-        List<Map<String, String>> mailList = sheet.getSheets().get("Mailovi");
+    public void createNewAgents(Spreadsheet spreadsheet){
+        List<Map<String, String>> mailList = spreadsheet.getSheets().get("Mailovi");
 
         for (Map<String, String> item : mailList) {
-            for (String email: item.values()) {
+            for (String key : item.keySet()) {
+                String email = item.get(key);
                 if(!Agents.exists(email)){
                     Agents.createAgent(new Moli(email));
                 }
