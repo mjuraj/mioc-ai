469c56038cae7ea5a10e261236c2f65a2518a526
markozele1 <marko.zelenovicc@gmail.com>
2023-02-27 17:32:32 +0100

    rijesio sve za ravnatelja osim teksta maila, treba provjeriti je li rule dobro napisan

--- services/rule_engine/src/main/java/agents/Ravnatelj.java
+++ services/rule_engine/src/main/java/agents/Ravnatelj.java
@@ -7,12 +7,16 @@ import java.util.List;
 import com.mindsmiths.emailAdapter.NewEmail;
 import com.mindsmiths.emailAdapter.EmailAdapterAPI;
 
+import java.time.LocalDateTime;
+
 public class Ravnatelj extends Agent {
     public static String ID = "RAVNATELJ";
 
     public Ravnatelj() {
         id = ID;
     }
+
+    public LocalDateTime lastEmailSentTime;
 
     public void sendEmail(List<String> recipients, String emailTitle, String emailText) throws IOException {
         NewEmail email = new NewEmail();
