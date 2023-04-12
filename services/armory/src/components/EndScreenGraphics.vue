ffc77e1f6db55489c5a3dabb845847b98d30cd80
TheChoconut <thechoconut@outlook.com>
2023-04-12 20:04:28 +0200

    apply new design to armory

---
+++ services/armory/src/components/EndScreenGraphics.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <img src="/public/mioc.png" width="64px" height="64px" />
+    <img src="/public/sovica.png" class="moli-bottom" />
+    <div class="text-headline-bottom">Voli te Moli!</div>
+  </div>
+</template>
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "EndScreenGraphics",
+  props: {},
+};
+</script>
---
+++ services/armory/src/components/StartScreenGraphics.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="startscreen">
+    <div class="background"></div>
+    <img src="/public/mioc.png" width="64px" height="64px" />
+    <img src="/public/sovica.png" class="moli-right" />
+    <div class="text-headline">Bok!</div>
+    <div class="text-headline2">Ja sam Moli,<br />AI asistent<br />MIOC-a.</div>
+    <div class="text-description">
+      Ovdje sam da pomognem poboljšati tvoju školu. Od mene ćeš dobivati mail s
+      kratkom anketom svaka tri mjeseca.
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "StartScreenGraphics",
+  props: {},
+};
+</script>
