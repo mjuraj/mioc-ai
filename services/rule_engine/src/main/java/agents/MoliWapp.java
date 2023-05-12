b12ddf83af910b5adbd578ed4e669a51cbcdf589
TheChoconut <thechoconut@outlook.com>
2023-05-12 19:47:01 +0200

    Initial work on MoliWapp, don't add a phone entry if phone number is null.

--- services/rule_engine/src/main/java/agents/MoliWapp.java
+++ services/rule_engine/src/main/java/agents/MoliWapp.java
@@ -1,6 +1,24 @@
 package agents;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.mindsmiths.armory.ArmoryAPI;
+import com.mindsmiths.armory.Screen;
+import com.mindsmiths.armory.component.Description;
+import com.mindsmiths.armory.component.CustomComponent;
+import com.mindsmiths.armory.component.Header;
+import com.mindsmiths.armory.component.Image;
+import com.mindsmiths.armory.component.SubmitButton;
+import com.mindsmiths.armory.component.TextArea;
+import com.mindsmiths.armory.component.Title;
+import com.mindsmiths.gpt3.GPT3AdapterAPI;
+import com.mindsmiths.infobipAdapter.InfobipAdapterAPI;
 import com.mindsmiths.ruleEngine.model.Agent;
+import com.mindsmiths.ruleEngine.util.Log;
+import com.mindsmiths.sdk.utils.Utils;
+
+import config.Settings;
 import lombok.*;
 
 @Data
@@ -8,8 +26,101 @@ import lombok.*;
 @NoArgsConstructor
 public class MoliWapp extends Agent {
 
+    String gender;
+    Integer age;
+    Integer rating;
+    String feedback;
+    String response;
+
+    public boolean reminderSent;
+    public LocalDateTime lastNpsSent;
+
+    public String getArmoryUrl() {
+        return Settings.ARMORY_SITE_URL + "/" + getConnection("armory");
+    }
+
     public MoliWapp(String phone) {
         this.id = phone;
-        //setConnection("", phone);
-    } 
+        setConnection("infobip", phone);
+        setConnection("armory", Utils.randomString());
+    }
+
+    public MoliWapp(String phone, String gender, Integer age) {
+        this(phone);
+        // TODO: Maybe you will need this to use setGender() or setAge()
+        // to trigger rules.
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public void askForSurveyResponse() {
+        String intro = String.format("You are Moli, an AI agent. Write me a simple, one or two sentences long, response that must be in Croatian language where you thank a highschool student for finishing the survey. Make it very friendly, but generic. Do not add any names.");
+        simpleGPT3Request(intro);
+    }
+
+    public void simpleGPT3Request(String prompt) {
+        Log.info("Prompt for GPT-3:\n" + prompt);
+        GPT3AdapterAPI.complete(
+            prompt, // input prompt
+            "text-davinci-001", // model
+            150, // max tokens
+            0.9, // temperature
+            1.0, // topP
+            1, // N
+            null, // logprobs
+            false, // echo
+            List.of("Human:", "Moli:"), // STOP words
+            0.6, // presence penalty
+            0.0, // frequency penalty
+            1, // best of
+            null // logit bias
+        );
+    }
+
+    // TODO: A generic is needed here for all types of messages,
+    // media, buttons etc.
+    public void sendWhatsappTextMessage(String message) {
+        InfobipAdapterAPI.sendWhatsappTextMessage(getConnection("infobip"), message);
+    }
+
+    public void showNPSFlow() {
+        ArmoryAPI.show(
+            getConnection("armory"),
+            new Screen("Welcome")
+                .add(new Header("logo.png", false))
+                .add(new Image("public/sovica.png", true))
+                .add(new Title("Prvo kratko upoznavanje, a onda želim čuti tvoje mišljenje ;)"))
+                .add(new SubmitButton("welcomeStarted", "Idemo!", "askForGender")),
+            new Screen("askForGender")
+                .add(new Header("logo.png", true)) 
+                .add(new Title("Jesi li miočanin ili miočanka?")) 
+                .add(new Image("public/sovica.png", true))
+                .add(new SubmitButton("askForGenderStartedm", "Miočanin", "askForAge")) //uhvati podatke i salji Ravnatelj agentu
+                .add(new SubmitButton("askForGenderStartedf", "Miočanka", "askForAge")),
+            new Screen("askForAge")
+                .add(new Header("logo.png", true)) 
+                .add(new Title("Koji si razred?"))
+                .add(new Image("public/sovica.png", true))
+                .add(new SubmitButton("askForAgeStarted1", "Prvi", "askForRating")) //uhvati podatke i salji Ravnatelj agentu
+                .add(new SubmitButton("askForAgeStarted2", "Drugi", "askForRating"))
+                .add(new SubmitButton("askForAgeStarted3", "Treći", "askForRating"))
+                .add(new SubmitButton("askForAgeStarted4", "Četvrti", "askForRating")),
+            new Screen("askForRating")
+                .add(new Header("logo.png", true)) 
+                .add(new Title("Kolika je vjerojatnost da bi preporučio MIOC frendu ili frendici?"))
+                .add(new Description("Označi odgovor na skali od 0 do 10. 0 znači da ne bi uopće preporučio, a 10 da bi sigurno preporučio."))
+                .add(new CustomComponent("Slider").addParam("inputId", "nps"))
+                .add(new SubmitButton("askForRatingStarted", "Idemo!", "askForFeedback")), //dodaj slider
+            new Screen("askForFeedback")
+                .add(new Header("logo.png", true))
+                .add(new Title("Zašto?"))
+                .add(new Description("Slobodno napiši  zašto si se odlučio za tu ocjenu i što možemo učiniti da bi ona bila bolja."))
+                .add(new TextArea("feedback", "Napiši svoj kometar..."))
+                .add(new SubmitButton("askForFeedbackStarted", "Pošalji", "endScreen")),
+            new Screen("endScreen")
+                .add(new Image("public/srce.png", false))
+                .add(new Title("Tvoj odgovor je poslan!"))
+                .add(new Description(response))
+        );
+    }
 }
