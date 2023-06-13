d1e5d35e6e4e924a81e0db8279462bd2a37058d7
TheChoconut <thechoconut@outlook.com>
2023-06-13 13:00:47 +0200

    upgrade project, fix dashboard-client

--- services/rule_engine/src/main/java/com/mindsmiths/dashboard/models/PersonInterface.java
+++
@@ -1,8 +0,0 @@
-package com.mindsmiths.dashboard.models;
-
-public interface PersonInterface {
-    String getId();
-    String getFirstName();
-    String getLastName();
-    String getPhoneNumber();
-}
